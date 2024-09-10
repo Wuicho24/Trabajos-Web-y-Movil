4ceb27e0bc3fd2b01a1c4434cd1d77c270e62c46
Wuicho24 <117499238+Wuicho24@users.noreply.github.com>
2024-09-09 22:25:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ $(document).ready(function() {
     $('#calcForm').submit(function(event) {
         event.preventDefault();
         
-        // Obtener valores de los inputs
+        
         var number1 = parseFloat($('#number1').val());
         var number2 = parseFloat($('#number2').val());
         var operation = $('#operation').val();
         var result = 0;
         
-        // Realizar la operación aritmética según la selección
+       
         if (operation === 'sumar') {
             result = number1 + number2;
         } else if (operation === 'restar') {
@@ -19,7 +19,7 @@ $(document).ready(function() {
             result = (number2 !== 0) ? number1 / number2 : 'Error: División por cero';
         }
         
-        // Mostrar el resultado en el span dentro del div correspondiente
+        
         $('#resultValue').text(result);
     });
 });
